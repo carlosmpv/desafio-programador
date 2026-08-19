568cc831c977bec346156c85c707db6ffaf9b2d2
carlosmpv <carlosmpvaldes@gmail.com>
2026-08-19 15:44:28 +0000

    Outra abordagem vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 
 const prisma = new PrismaClient({
-    adapter: new PrismaPg({ connectionString: process.env.POSTGRESQL_POSTGRES_URL })
+    adapter: new PrismaPg({ connectionString: process.env.POSTGRESQL_PRISMA_DATABASE_URL })
 });
 
 export { prisma };
